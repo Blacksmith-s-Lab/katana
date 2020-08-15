8a0376829a54af10737ec1d3a009e5eead2ff66f
Victor Tadashi <victor.t.y.z@hotmail.com>
2020-08-15 01:08:25 -0300

    add new feature
    Now you can pass arguments to show on notification
    
    arg1: Title
    arg2: Message
    arg3: Application Name
    arg4: Path to icon image (.png for Linux and .ico for Windows)

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-import platform
+import sys
+import collections
+
 from core.notify import Notify
 
 def main():
-    Notify().doNotify("Katana", "Hello World from Katana", "Katana", "./assets/katana-logo." + ('ico' if platform.system() == 'Windows' else 'png') )
+    arg_names = ['title', 'message', 'app_name', 'app_icon']
+
+    args = collections.namedtuple('data', arg_names)(
+        *(dict(zip(arg_names, sys.argv[1:])).get(arg, "") for arg in arg_names))
+
+    TITLE = args.title if args.title else "Katana"
+    MESSAGE = args.message if args.message else "Hello World from Katana"
+    APP_NAME = args.app_name if args.app_name else "Katana"
+    APP_ICON = args.app_name if args.app_name else "./assets/katana-logo.svg"
+
+    Notify().doNotify(TITLE, MESSAGE, APP_NAME, APP_ICON)
     
 
 if __name__ == "__main__":
